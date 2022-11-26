c71ceb02b614b20067e49d1e9b805129b116e11b
0x00b7 <0x00b7@gmail.com>
2022-11-26 15:05:58 +0100

    ~

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import './assets/fonts/FA/all.min.js'
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+    />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  background-color: blue;
+}
+
+/* ROUTER VIEW */
+header + div {
+  background-color: rgba(166, 255, 0, 0.3);
+}
+
+.logo {
+  top: 15px;
+  display: block;
+  margin: 0 auto;
+  width: auto;
+  height: 9rem;
+}
+
+nav {
+  background-color: red;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: left;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    top: 0;
+    width: auto;
+    height: 5.9rem;
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  /* ONLY-BROWSER */
+}
+
+</style>
