9fb556217821a76b5149a6b3b93eaaa11f44d1fc
0x00b7 <0x00b7@gmail.com>
2022-11-26 20:18:23 +0100

    ~

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,78 @@
 <template>
   <div class="container">
     <div class="welcome">
-      <h1>
-        MAKE THE WWW <br />
-        YOUR <span id="u" class="blueviolet">TERRITORY!</span>
+      <h1> MAKE THE WWW <br /> YOUR <span id="u" class="blueviolet">TERRITORY!</span>
       </h1>
     </div>
     <div class="definition">
-      <h1>Netox not only creates your presentation but also provides all the other components to deliver your site.</h1>
-      <h2>We will</h2>
+      <h1> Netox not only creates your presentation but also provides all the other components to deliver your site. </h1>
+      <h2>WE ARE RESPONSIBLE FOR</h2>
+      <ul class="fa-ul">
+        <li>
+          <span class="fa-li">
+            <i class="fa-sharp fa-rectangle-code"></i>
+          </span>PROGRAMMING
+        </li>
+        <li>
+          <span class="fa-li">
+            <i class="fa-sharp fa-wifi"></i>
+          </span>HOSTING
+        </li>
+        <li>
+          <span class="fa-li">
+            <i class="fa-sharp fa-link"></i>
+          </span>DOMAIN-LINKING
+        </li>
+        <li>
+          <span class="fa-li">
+            <i class="fa-sharp fa-magnifying-glass-location"></i>
+          </span>SEARCH-ENGINE-OPTIMIZATION (SEO)
+        </li>
+        <li>
+          <span class="fa-li">
+            <i class="fa-sharp fa-shield-keyhole"></i>
+          </span>SERVICE PROTECTION
+        </li>
+      </ul>
     </div>
   </div>
 </template>
-
 <style scoped>
-div {
-  width: 100%;
-  background-color: rgba(0, 110, 255, 0.5);
-}
-.container {
-  padding: 1vh 1vh;
-  font-size: 3vh;
-}
 
-.welcome {
-  line-height: 1;
-}
-
-.definition {
-  font-size: 1.5vh;
-  line-height: 1;
-}
-
-@media (min-width: 1024px) {
-  .container {
-    display: flex;
+  ul {
+    padding: 1% 0;
+    left: -1%
   }
-}
+
+  li {
+    line-height: 1.5;
+  }
+
+  div {
+    width: 100%;
+  }
+
+  .container {
+    padding: 1vh 1vh;
+    font-size: 4vh;
+  }
+
+  .welcome {
+
+  }
+
+  .definition {
+    font-size: 2vh;
+  }
+  .definition h2 {
+    margin-top: 1rem;
+    text-align: center;
+    border-bottom: 1px solid var(--color-border);
+  }
+
+  @media (min-width: 1024px) {
+    .container {
+      display: flex;
+    }
+  }
 </style>
