8f328f81fea2c88ce4ca48172a43a252b311283d
0x00b7 <0x00b7@gmail.com>
2022-11-27 13:19:31 +0100

    ~

---
+++ src/views/Env.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="env">
+      <h1>This page is under construction..</h1>
+    </div>
+  </div>
+</template>
+<style scoped>
+  div {
+    width: 100%;
+  }
+
+  .container {
+    padding: 1vh 1vh;
+    font-size: 4vh;
+  }
+
+  @media (min-width: 1024px) {
+    .container {
+      display: flex;
+    }
+  }
+</style>
