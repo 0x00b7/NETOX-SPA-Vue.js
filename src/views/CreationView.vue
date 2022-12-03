fba3b4460f38421ae19b80824c18f15f60a2cfeb
0x00b7 <0x00b7@gmail.com>
2022-12-03 20:25:06 +0100

    ~

--- src/views/CreationView.vue
+++ src/views/CreationView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
+     <div class="section">
+        <div class="">
 
+        </div>
+     </div>
 
+     <div class="section">
+        <div class="">
+
+        </div>
+     </div>
   </div>
 </template>
 
-
-
-
 <style scoped>
 /* HEAD-STUB */
-.add {
-  padding: 2vh 0;
-}
 .container {
   border: 1px solid rgb(10, 10, 10);
   background-color: rgba(15, 15, 15, 1);
@@ -23,96 +26,16 @@
   display: flex;
   padding: 2vh 5vh;
 }
-.section:nth-child(2) {
-  padding: 2vh 2vh;
-  justify-content: center;
-  align-items: center;
-}
-
-/* THE WWW.. */
-.home {
-  font-size: 42px;
-  width: 100%;
-}
-
-/* WE ARE RESPONSIBLE FOR.. */
-.define {
-  width: 100%;
-}
-.define h2 {
-  margin-top: 1rem;
-  text-align: center;
-  border-bottom: 1px solid var(--color-border);
-}
-ul {
-  padding: 10px 0;
-}
-li {
-  line-height: 1.5;
-}
-
-/* THE DIFFERENCE */
-.add h1 {
-  font-size: 42px;
-}
-.feature-box {
-  width: 100%;
-  height: 270px;
-  background-color: rgba(10, 10, 10, 1);
-  text-align: center;
-  border: 1px solid rgb(5, 5, 5);
-  margin: 0 10px 0 0;
-  padding: 20px 50px;
-}
-.feature-box p:nth-child(1) {
-  font-size: 30px;
-}
-.feature-box p:nth-child(3) {
-  max-width: 200px;
-  word-wrap: break-word;
-}
-.feature-box svg {
-  margin: 1.5rem 0;
-  font-size: 72px;
-}
 
 /* W1024x ~ iPad */
 @media (max-width: 64em) {
   .section {
       display: grid;
   }
-  .section:nth-child(2) {
-      display: grid;
-      grid-template-columns: auto auto;
-      grid-column-gap: 20px;
-  }
-  .define {
-      font-size: 16px;
-  }
-  .home {
-      font-size: 42px;
-  }
-  .feature-box {
-      margin: auto 0 20px;
-      height: 280px;
-  }
 }
 
 /* W512x ~ iPhone */
 @media (max-width: 32em) {
-  .section:nth-child(2) {
-      display: grid;
-      grid-template-columns: inherit;
-  }
-  .home {
-      white-space: nowrap;
-      font-size: 4vh;
-  }
-  .define {
-      font-size: 2vh;
-  }
-  .feature-box {
-      height: 265px;
-  }
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,19 @@
        <div class="section">
           <div class="home">
              <h1>MAKE THE <br> WWW YOUR <br> <span id="UL" class="gardient">TERRITORY!</span></h1>
+             <div class="button">
+
+                <i class="fa-sharp fa-angle-right fa-fade"></i>
+                <i class="fa-sharp fa-angle-right fa-fade"></i>
+                <i class="fa-sharp fa-angle-right fa-fade"></i>
+                <RouterLink to="/creation">
+                   <div>
+                      <p>ORDER&nbsp;</p>
+                      <p>NOW</p>
+                   </div>
+                </RouterLink>
+
+             </div>
           </div>
           <div class="define">
              <h1>Netox not only creates your presentation but also provides all the other components to deliver your site.</h1>
@@ -64,119 +77,164 @@
              </div>
           </div>
        </div>
-
        <!-- Section-Text -->
-
-       
     </div>
  </template>
 
 
-<style scoped>
-/* HEAD-STUB */
-.add {
-    padding: 2vh 0;
-}
-.container {
-    border: 1px solid rgb(10, 10, 10);
-    background-color: rgba(15, 15, 15, 1);
-}
+ <style scoped>
+ /* HEAD-STUB */
+ .add {
+     padding: 2vh 0;
+ }
+ .container {
+     border: 1px solid rgb(10, 10, 10);
+     background-color: rgba(15, 15, 15, 1);
+ }
+ 
+ /* SKELETON */
+ .section {
+     display: flex;
+     padding: 2vh 5vh;
+ }
+ .section:nth-child(2) {
+     padding: 2vh 2vh;
+     justify-content: center;
+     align-items: center;
+ }
+ 
+ /* THE WWW.. */
+ .home {
+     font-size: 42px;
+     width: 100%;
+ }
+ 
+ .button svg {
+     color: green;
+ }
+ 
+ .button {
+     margin-top: 10px;
+     width: 260px;
+     font-size: 80%;
+     padding: 5px 0px;
+     display: flex;
+ }
 
-/* SKELETON */
-.section {
+ .button div {
+    margin: 0 5px;
     display: flex;
-    padding: 2vh 5vh;
-}
-.section:nth-child(2) {
-    padding: 2vh 2vh;
-    justify-content: center;
-    align-items: center;
-}
+ }
+ .button p {
+    top: 1px;
+ } 
 
-/* THE WWW.. */
-.home {
-    font-size: 42px;
-    width: 100%;
-}
+ .button div:hover > p:nth-child(2) {
+     animation-name: fa-shake;
+     animation-delay: 0s;
+     animation-direction: normal;
+     animation-duration: 2000ms;
+     animation-iteration-count: infinite;
+     animation-timing-function: linear;
+ }
 
-/* WE ARE RESPONSIBLE FOR.. */
-.define {
-    width: 100%;
-}
-.define h2 {
-    margin-top: 1rem;
-    text-align: center;
-    border-bottom: 1px solid var(--color-border);
-}
-ul {
-    padding: 10px 0;
-}
-li {
-    line-height: 1.5;
-}
-
-/* THE DIFFERENCE */
-.add h1 {
-    font-size: 42px;
-}
-.feature-box {
-    width: 100%;
-    height: 270px;
-    background-color: rgba(10, 10, 10, 1);
-    text-align: center;
-    border: 1px solid rgb(5, 5, 5);
-    margin: 0 10px 0 0;
-    padding: 20px 50px;
-}
-.feature-box p:nth-child(1) {
-    font-size: 30px;
-}
-.feature-box p:nth-child(3) {
-    max-width: 200px;
-    word-wrap: break-word;
-}
-.feature-box svg {
-    margin: 1.5rem 0;
-    font-size: 72px;
-}
-
-/* W1024x ~ iPad */
-@media (max-width: 64em) {
-    .section {
-        display: grid;
-    }
-    .section:nth-child(2) {
-        display: grid;
-        grid-template-columns: auto auto;
-        grid-column-gap: 20px;
-    }
-    .define {
-        font-size: 16px;
-    }
-    .home {
-        font-size: 42px;
-    }
-    .feature-box {
-        margin: auto 0 20px;
-        height: 280px;
-    }
-}
-
-/* W512x ~ iPhone */
-@media (max-width: 32em) {
-    .section:nth-child(2) {
-        display: grid;
-        grid-template-columns: inherit;
-    }
-    .home {
-        white-space: nowrap;
-        font-size: 4vh;
-    }
-    .define {
-        font-size: 2vh;
-    }
-    .feature-box {
-        height: 265px;
-    }
-}
-</style>
+ .button svg:nth-child(1) {
+     animation-delay: 500ms;
+     --fa-animation-duration: 2s;
+     --fa-fade-opacity: 0.1;
+ }
+ .button svg:nth-child(2) {
+     animation-delay: 1000ms;
+     --fa-animation-duration: 2s;
+     --fa-fade-opacity: 0.1;
+ }
+ .button svg:nth-child(3) {
+     animation-delay: 1500ms;
+     --fa-animation-duration: 2s;
+     --fa-fade-opacity: 0.1;
+ }
+ 
+ /* WE ARE RESPONSIBLE FOR.. */
+ .define {
+     width: 100%;
+ }
+ .define h2 {
+     margin-top: 1rem;
+     text-align: center;
+     border-bottom: 1px solid var(--color-border);
+ }
+ ul {
+     padding: 10px 0;
+ }
+ li {
+     line-height: 1.5;
+ }
+ 
+ /* THE DIFFERENCE */
+ .add h1 {
+     font-size: 42px;
+ }
+ .feature-box {
+     width: 100%;
+     height: 270px;
+     background-color: rgba(10, 10, 10, 1);
+     text-align: center;
+     border: 1px solid rgb(5, 5, 5);
+     margin: 0 10px 0 0;
+     padding: 20px 50px;
+ }
+ .feature-box p:nth-child(1) {
+     font-size: 30px;
+ }
+ .feature-box p:nth-child(3) {
+     max-width: 200px;
+     word-wrap: break-word;
+ }
+ .feature-box svg {
+     margin: 1.5rem 0;
+     font-size: 72px;
+ }
+ 
+ /* W1024x ~ iPad */
+ @media (max-width: 64em) {
+     .section {
+         display: grid;
+     }
+     .section:nth-child(2) {
+         display: grid;
+         grid-template-columns: auto auto;
+         grid-column-gap: 20px;
+     }
+     .define {
+         font-size: 16px;
+     }
+     .home {
+         font-size: 42px;
+     }
+     .button {
+         margin-bottom: 10px;
+     }
+     .feature-box {
+         margin: auto 0 20px;
+         height: 280px;
+     }
+ }
+ 
+ /* W512x ~ iPhone */
+ @media (max-width: 32em) {
+     .section:nth-child(2) {
+         display: grid;
+         grid-template-columns: inherit;
+     }
+     .home {
+         white-space: nowrap;
+         font-size: 4vh;
+     }
+     .define {
+         font-size: 2vh;
+     }
+     .feature-box {
+         height: 265px;
+     }
+ }
+ </style>
