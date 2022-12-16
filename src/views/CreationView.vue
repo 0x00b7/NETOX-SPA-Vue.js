bef3dca455ffbc1b0bbc7d1c10a06446698528ab
0x00b7 <0x00b7@gmail.com>
2022-12-16 18:42:10 +0100

    v1.3

--- src/views/CreationView.vue
+++ src/views/CreationView.vue
@@ -11,14 +11,28 @@ const scrollRef = ref({});
 const onEnter = (el) => {
   anime({
   targets: el,
-  keyframes: [
-    {translateY: -40},
-    {translateX: 250},
-    {translateY: 40},
-    {translateX: 0},
-    {translateY: 0}
+  translateX: [
+    { value: 250, duration: 1000, delay: 500 },
+    { value: 0, duration: 1000, delay: 500 }
   ],
-  duration: 4000,
+  translateY: [
+    { value: -40, duration: 500 },
+    { value: 40, duration: 500, delay: 1000 },
+    { value: 0, duration: 500, delay: 1000 }
+  ],
+  scaleX: [
+    { value: 4, duration: 100, delay: 500, easing: 'easeOutExpo' },
+    { value: 1, duration: 900 },
+    { value: 4, duration: 100, delay: 500, easing: 'easeOutExpo' },
+    { value: 1, duration: 900 }
+  ],
+  scaleY: [
+    { value: [1.75, 1], duration: 500 },
+    { value: 2, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+    { value: 1, duration: 450 },
+    { value: 1.75, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+    { value: 1, duration: 450 }
+  ],
   easing: 'easeOutElastic(1, .8)',
   loop: true
 });
@@ -68,7 +82,7 @@ onUnmounted(() => {
 .box {
   background-color: lime;
   width: 20px;
-  height: 20px;
+  top: 25%;
 }
 
 
