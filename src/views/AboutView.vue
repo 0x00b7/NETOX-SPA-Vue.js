119028e0ef0fbb8543e3514d4ede0a0bef76b0d7
0x00b7 <0x00b7@gmail.com>
2022-12-18 17:12:02 +0100

    v1.6

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
     <div class="container">
        <div class="section">
           <img alt="Loading..">
@@ -11,18 +11,6 @@
        </div>
     </div>
  </template>
-
- <script>
-import Loading from "../components/Fetch/LoadingSkeleton.vue";
-import AboutFetch from "../components/Fetch/AboutFetch.vue";
-
-export default {
-  components: {
-    Loading: Loading,
-    AboutFetch: AboutFetch,
-  },
-}
-</script>
  
 <style scoped>
 .container {
@@ -31,14 +19,6 @@ export default {
 
 .section {
     width: 100%;
-}
-
-.section h1 {
-    margin-bottom: 0.5rem;
-}
-
-.section h2 {
-    margin-bottom: 2rem;
 }
 
 img {
@@ -51,14 +31,15 @@ img {
     height: 100%;
 }
 
-.section h2 {
-    margin-bottom: 2.5rem;
-}
-
-
+/* W1024x ~ iPad */
 @media (max-width: 64em) {
     .container {
         display: grid;
+    }
+
+    img {
+        content: url(../assets/images/admin_small.png);
+        width: 75%;
     }
 
     .section:nth-child(1) {
@@ -68,6 +49,21 @@ img {
 
     .section:nth-child(2) {
         padding: 1rem;
+    }
+
+}
+
+/* W512x ~ iPhone */
+@media (max-width: 32em) {}
+
+@keyframes av_admin-fade {
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+        margin-left: 0%;
     }
 }
 </style>
