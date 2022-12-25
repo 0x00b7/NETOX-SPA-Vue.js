605ecfa67ac3b1741e125d6a23542c9d6703b2c4
0x00b7 <0x00b7@gmail.com>
2022-12-25 19:05:26 +0100

    v1.6

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,76 @@
+<script>
+import anime from "animejs";
+
+export default {
+    mounted() {
+        var el = document.querySelectorAll('.N, .E, .T, .O, .X')
+        anime({
+            targets: el,
+            opacity: [0, 1],
+            translateX: [-270, 0],
+            easing: 'easeOutElastic(1, 1)',
+            scaleX: [{
+                    value: 5,
+                    duration: 25,
+                    easing: 'easeOutExpo'
+                },
+                {
+                    value: 1,
+                    duration: 900
+                }
+            ],
+            scaleY: [{
+                    value: [1.75, 1],
+                    duration: 450
+                },
+                {
+                    value: 2,
+                    duration: 100,
+                    delay: 750,
+                    easing: 'easeOutExpo'
+                },
+                {
+                    value: 1,
+                    duration: 450
+                },
+            ],
+            delay: anime.stagger(100, {
+                direction: 'reverse',
+            })
+        });
+        anime({
+            targets: '.dot',
+            easing: 'spring(0, 20, 10, 0)',
+            delay: 250,
+            opacity: [0, 1],
+            translateX: [100, 0],
+            scaleY: [{
+                    value: [2, 1],
+                    duration: 450,
+                },
+            ],
+        });
+        anime({
+            targets: '.ws',
+            easing: 'spring(0, 20, 10, 0)',
+            delay: 900,
+            opacity: [0, 1],
+        })
+    },
+}
+</script>
+
 <template>
     <header>
+        <div class="sign">
+            <div class="N">N</div>
+            <div class="E">E</div>
+            <div class="T">T</div>
+            <div class="O">O</div>
+            <div class="X">X</div>
+            <div class="dot">.</div>
+            <div class="ws">ws</div>
+        </div>
        <div class="wrapper">
           <nav>
              <RouterLink to="/">Home</RouterLink>
@@ -13,6 +84,18 @@
 <style scoped>
 header {
     display: grid;
+}
+
+header .sign {
+    display: flex;
+    padding-top: 5px;
+    white-space: nowrap;
+    letter-spacing: 0.1rem;
+}
+
+header .sign div {
+    font-weight: 600;
+    font-size: 3rem;
 }
 
 header .wrapper {
@@ -29,12 +112,12 @@ nav {
     width: 100%;
     font-size: 24px;
     text-align: left;
-    margin: 15px 0px 15px 0px;
+    margin: 5px 0px 15px 0px;
 }
 
 nav a {
     display: inline-block;
-    padding: 0 1rem;
+    padding: 0 1.5rem;
     border-left: 1px solid var(--color-border);
 }
 
@@ -45,8 +128,12 @@ nav a:first-of-type {
 /* W1024x ~ iPad */
 @media (max-width: 64em) {
     header {
-        line-height: 1.5;
+        line-height: 1.2;
         max-height: 100vh;
+    }
+    
+    header .sign {
+        justify-content: center;
     }
 
     nav {
--- src/views/dev.vue
+++ src/views/dev.vue
@@ -1,32 +1,36 @@
 <template>
     <div class="container">
-       <div v-if="loading" class="placeholder-wave">
-          <div class="placeholder"></div>
-       </div>
-       <img v-else :src="imageUrl" />
+      <div v-if="loading" class="placeholder-wave">
+        <div class="placeholder"></div>
+      </div>
+      <img v-else ref="image" :src="imageUrl" @load="onImageLoaded" />
     </div>
- </template>
- <script>
-    export default {
-      data() {
-        return {
-          loading: true,
-          imageUrl: ''
-        }
-      },
-      methods: {
-        async fetchImage() {
-          const response = await fetch('https://www.sony.de/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960')
-          this.imageUrl = await response.url
-          this.loading = false
-        }
-      },
-      mounted() {
-        this.fetchImage()
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        loading: true,
+        imageUrl: ''
       }
+    },
+    methods: {
+      async fetchImage() {
+        const response = await fetch('https://www.sony.de/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960')
+        this.imageUrl = await response.url
+      },
+      onImageLoaded() {
+        this.loading = false
+      }
+    },
+    mounted() {
+      this.fetchImage()
     }
- </script>
- 
+  }
+  </script>
+  
+  
 <style>
 .placeholder-wave {
     -webkit-mask-image: linear-gradient(130deg, #000 55%, rgba(0, 0, 0, 0.8) 75%, #000 95%);
