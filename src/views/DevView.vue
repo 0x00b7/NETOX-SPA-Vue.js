bd2b5b4e3d723423c06428590232c137d24488d6
0x00b7 <0x00b7@gmail.com>
2023-01-14 21:24:03 +0100

    v2.0

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,30 +1,19 @@
 <template>
-	<form
-	  @submit.prevent="handleSubmit">
-	  <label v-for="(panelist, index) in panelists" :key="index">
-		<input
-		  type="radio"
-		  name="panelist"
-		  @input="ev => form.askPerson = ev.target.value"
-		  :value="panelist"
-		  :checked="form.askPerson === panelist"
-		/>
-		<span>{{ panelist }}</span>
-	  </label>
-	  <button @click="handleSubmit()">X</button>
-	</form>
-  </template>
+<form name="ask-question" netlify netlify-honeypot="bot-field">
+	<textarea :value="form.askPerson" name="question"></textarea>
+	<button @click="handleSubmit()" type="submit"></button>
+</form>
+</template>
+
   <script>
   import axios from "axios";
   
   export default {
-	name: "QAForm",
 	data () {
 	  return {
 		form: {
 		  askPerson: ""
 		},
-		panelists: ['Evan You', 'Chris Fritz'],
 	  }
 	},
 	methods: {
