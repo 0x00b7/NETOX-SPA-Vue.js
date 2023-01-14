3a142a698ac8a21ddf8f0a62a86c4d5c49164522
0x00b7 <0x00b7@gmail.com>
2023-01-14 21:37:20 +0100

    v2.0

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,42 +1,54 @@
 <template>
-<form name="ask-question" netlify netlify-honeypot="bot-field">
-	<textarea :value="form.askPerson" name="question"></textarea>
-	<button @click="handleSubmit()" type="submit"></button>
-</form>
-</template>
 
-  <script>
-  import axios from "axios";
-  
-  export default {
-	data () {
-	  return {
-		form: {
-		  askPerson: ""
-		},
-	  }
+	<form name="ask-question" netlify netlify-honeypot="bot-field" @submit.prevent="handleSubmit">
+	   <textarea v-model="form.askPerson" name="askPerson"></textarea>
+	   <button @click="handleSubmit()">Send</button>
+	</form>
+
+ </template>
+ 
+<script>
+import axios from "axios";
+
+export default {
+
+
+	data() {
+		return {
+			form: {
+				askPerson: ''
+			}
+		}
 	},
-	methods: {
-	  encode (data) {
-		return Object.keys(data)
-		  .map(
-			key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-		  )
-		  .join("&");
-	  },
-	  handleSubmit () {
-		const axiosConfig = {
-		  header: { "Content-Type": "application/x-www-form-urlencoded" }
-		};
-		axios.post(
-		  "/",
-		  this.encode({
-			"form-name": "ask-question",
-			...this.form
-		  }),
-		  axiosConfig
-		);
-	  }
-	}
-  }
-  </script>
+
+	mounted() {
+		setTimeout(()=>{console.log(this.form.askPerson)}, 5000)
+	},
+
+    methods: {
+        encode(data) {
+            return Object.keys(data)
+                .map(
+                    key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+                )
+                .join("&");
+        },
+
+        handleSubmit() {
+            const axiosConfig = {
+                header: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            };
+            axios.post(
+                "/",
+                this.encode({
+                    "form-name": "ask-question",
+                    ...this.form
+                }),
+                axiosConfig
+            );
+        }
+    }
+}
+</script>
