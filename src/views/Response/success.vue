da0cff818be15dcda6763dab1f31c2b635939fef
0x00b7 <0x00b7@gmail.com>
2023-01-16 20:31:37 +0100

    v2.5

--- src/views/Response/success.vue
+++ src/views/Response/success.vue
@@ -1,11 +1,36 @@
 <template>
     <div class="success">
-       <h1>Thank you! your Submission got applied! :-)</h1>
-    </div>
+        <div class="notify">
+            <h1>Thanks!</h1>
+            <i class="fa-sharp fa-solid fa-thumbs-up"></i>
+            <h3>We will contact you shortly regarding further steps. :)</h3>
+        </div>
+     </div>
 </template>
 
 <style>
 .success {
+    display: grid;
+    height: 300px;
+    margin: 2rem;
+}
+
+.notify {
+    text-align: center;
+    justify-self: center;
+    margin: auto 0;
+}
+
+.notify h1 {
+    font-weight: 700;
+}
+
+.notify svg {
     padding: 2rem;
+    font-size: 5rem;
+}
+
+.notify h3 {
+    font-weight: 200;
 }
 </style>
