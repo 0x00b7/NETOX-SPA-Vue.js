afb8fa090e92cef2e7c8d2bb018eb20250963fd6
0x00b7 <0x00b7@gmail.com>
2023-01-23 13:19:27 +0100

    v2.5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,20 +7,19 @@ export default {
         OS: OS,
     },
     mounted() {
-        anime({
-            targets: '.mark',
-			easing: 'linear',
-            duration: 800,
-			loop: true,
-        }),
 		anime({
+			targets: '.char',
+			opacity: [0, 1],
+			delay: anime.stagger(250)
+		})
+        anime({
             targets: '.fa-angle-right',
             opacity: [1, 0.2, 1],
-			easing: 'easeInCubic',
-			direction: "normal",
+            easing: 'easeInCubic',
+            direction: "normal",
             duration: 1000,
-			loop: true,
-			delay: anime.stagger(200)
+            loop: true,
+            delay: anime.stagger(200)
         })
     },
     methods: {
@@ -38,7 +37,14 @@ export default {
 	   <!-- Section I -->
 	   <div class="section">
 		  <div class="home">
-			 <h1>WE'LL BUILD YOUR VISION, START YOUR <span class="mark">JOURNEY</span> NOW!</h1>
+			 <h1>WE'LL BUILD YOUR VISION, START YOUR 
+				<span class="char">J</span>
+				<span class="char">O</span>
+				<span class="char">U</span>
+				<span class="char">R</span>
+				<span class="char">N</span>
+				<span class="char">E</span>
+				<span class="char">Y</span> NOW!</h1>
 			 <div class="button">
 				<svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fass" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M301.3 256l-22.6 22.6-160 160L96 461.3 50.7 416l22.6-22.6L210.7 256 73.4 118.6 50.7 96 96 50.7l22.6 22.6 160 160L301.3 256z"></path></svg>
 				<svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fass" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M301.3 256l-22.6 22.6-160 160L96 461.3 50.7 416l22.6-22.6L210.7 256 73.4 118.6 50.7 96 96 50.7l22.6 22.6 160 160L301.3 256z"></path></svg>
@@ -237,8 +243,8 @@ li {
 	width: 100%;
 	height: 270px;
 	background-color: rgba(10, 10, 10, 1);
-	border: 0.3rem solid crimson;
 	text-align: center;
+	border-radius: 1rem;
 	margin: 0 0.5rem 0 0;
 	padding: 15px 15px;
 	-webkit-box-pack: center;
