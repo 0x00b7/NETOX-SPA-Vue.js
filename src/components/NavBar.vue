5ccb91e8b263940b61585e95278ba84e8e138321
0x00b7 <0x00b7@gmail.com>
2023-02-02 23:36:22 +0100

    v3

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,72 +1,66 @@
 <template>
   <header>
-      <div class="mobile-nav">
-          <ul class="fa-ul">
+     <div class="container">
+        <button class="mobileBtn" @click="toggleMobile">X</button>
+        <div id="mobile-nav" class="mobile-nav">
+          <ul>
               <li>
                   <span class="fa-li">
                       <i class="fa-solid fa-earth-americas"></i>
                   </span>
-                  <RouterLink @click="toggleNavbar" to="/">Home</RouterLink>
+                  <RouterLink to="/">Home</RouterLink>
               </li>
               <li>
                   <span class="fa-li">
                       <i class="fa-solid fa-wand-magic-sparkles"></i>
                   </span>
-                  <RouterLink @click="toggleNavbar" to="/Service">Service</RouterLink>
+                  <RouterLink to="/Service">Service</RouterLink>
               </li>
               <li>
                   <span class="fa-li">
-                      <i class="fa-solid fa-book-user"></i>
+                      <i class="fa-solid fa-comments-question"></i>
                   </span>
-                  <RouterLink @click="toggleNavbar" to="/FAQs">FAQs</RouterLink>
+                  <RouterLink to="/FAQs">FAQs</RouterLink>
               </li>
               <li>
                   <span class="fa-li">
                       <i class="fa-solid fa-person-rays"></i>
                   </span>
-                  <RouterLink @click="toggleNavbar" to="/About">About</RouterLink>
+                  <RouterLink to="/About">About</RouterLink>
               </li>
-          </ul>
-      </div>
-      <div class="sign">
-          <div class="sign-background">
-              <div class="N">N</div>
-              <div class="E">E</div>
-              <div class="T">T</div>
-              <div class="O">O</div>
-              <div class="X">X</div>
-              <div class="dot">.</div>
-              <div class="ws">ws</div>
-              <div class="wrapper">
-                  <nav>
-                      <RouterLink to="/">Home</RouterLink>
-                      <RouterLink to="/Service">Service</RouterLink>
-                      <RouterLink to="/FAQs">FAQs</RouterLink>
-                      <RouterLink to="/About">About</RouterLink>
-                  </nav>
-              </div>
-          </div>
-          <div class="mobile-wrapper">
-              <button class="mobile-nav-toggle" @click="toggleNavbar">
-                  <i class="fa-solid fa-bars"></i>
-              </button>
-          </div>
-      </div>
+           </ul>
+        </div>
+        <div class="wrapper">
+           <div class="company">NETOX.ws</div>
+           <div class="desktop-nav">
+              <nav>
+                 <RouterLink to="/">Home</RouterLink>
+                 <RouterLink to="/Service">Service</RouterLink>
+                 <RouterLink to="/FAQs">FAQs</RouterLink>
+                 <RouterLink to="/About">About</RouterLink>
+              </nav>
+           </div>
+        </div>
+     </div>
   </header>
 </template>
 
 <script>
-import anime from "animejs";
+import anime from 'animejs';
 
 export default {
+    data() {
+        return {
+            openMobile: false,
+        }
+    },
     methods: {
-        toggleNavbar() {
+        toggleMobile() {
             const navbar = document.querySelector(".mobile-nav");
             if (navbar.style.display === "block") {
                 anime({
                     targets: navbar,
                     easing: 'easeInOutQuad',
-                    height: "0px",
                     opacity: [1, 0],
                     duration: 350,
                     complete: () => {
@@ -77,7 +71,6 @@ export default {
                 navbar.style.display = "block";
                 anime({
                     targets: navbar,
-                    height: "180px",
                     opacity: [0, 1],
                     easing: 'easeInOutQuad',
                     duration: 350,
@@ -86,210 +79,92 @@ export default {
         },
     },
     mounted() {
-        var sign = document.querySelectorAll(".N, .E, .T, .O, .X");
         anime({
-            targets: sign,
-            opacity: [0, 1],
-            translateX: [-270, 0],
-            easing: "easeOutElastic(1, 1)",
-            scaleX: [{
-                    value: 5,
-                    duration: 25,
-                    easing: "easeOutExpo",
-                },
-                {
-                    value: 1,
-                    duration: 900,
-                },
-            ],
-            scaleY: [{
-                    value: [1.75, 1],
-                    duration: 450,
-                },
-                {
-                    value: 2,
-                    duration: 100,
-                    delay: 750,
-                    easing: "easeOutExpo",
-                },
-                {
-                    value: 1,
-                    duration: 450,
-                },
-            ],
-            delay: anime.stagger(100, {
-                direction: "reverse",
-            }),
-        });
-        anime({
-            targets: ".dot",
-            easing: "spring(0, 20, 10, 0)",
             delay: 250,
+            translateX: [50, 0],
+            targets: '.company',
             opacity: [0, 1],
-            translateX: [100, 0],
-            scaleY: [{
-                value: [2, 1],
-                duration: 450,
-            }, ],
-        });
-        anime({
-            targets: ".ws",
-            easing: "spring(0, 20, 10, 0)",
-            delay: 900,
-            opacity: [0, 1],
-        });
-        anime({
-            targets: ".sign-background",
-            background: "linear-gradient(45deg, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
-            delay: 1000,
-            duration: 800,
-            easing: "easeOutElastic(1, 1)",
-        });
-    },
-};
+        })
+    }
+}
 </script>
 
 <style scoped>
 header {
-  display: -ms-grid;
-  display: grid;
+	display: -ms-grid;
+	display: grid;
+  background: rgba(0, 0, 0, 0.3);
 }
 
-header .sign {
-  white-space: nowrap;
-  font-size: 3rem;
+header .container {
+	white-space: nowrap;
+	font-size: 3rem;
 }
 
-header .sign .sign-background {
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  padding-left: 10px;
-  padding-right: 15px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  background: -webkit-gradient(
-    linear,
-    left bottom,
-    left top,
-    from(rgba(255, 255, 255, 0.2)),
-    to(rgba(255, 255, 255, 0.2))
-  );
-  background: -webkit-linear-gradient(
-    bottom,
-    rgba(255, 255, 255, 0.2) 0%,
-    rgba(255, 255, 255, 0.2) 100%
-  );
-  background: -moz-linear-gradient(
-    bottom,
-    rgba(255, 255, 255, 0.2) 0%,
-    rgba(255, 255, 255, 0.2) 100%
-  );
-  background: -o-linear-gradient(
-    bottom,
-    rgba(255, 255, 255, 0.2) 0%,
-    rgba(255, 255, 255, 0.2) 100%
-  );
-  background: -webkit-gradient(
-    linear,
-    left bottom, left top,
-    from(rgba(255, 255, 255, 0.2)),
-    to(rgba(255, 255, 255, 0.2))
-  );
-  background: linear-gradient(
-    0deg,
-    rgba(255, 255, 255, 0.2) 0%,
-    rgba(255, 255, 255, 0.2) 100%
-  );
-}
-
-header .sign .sign-background div {
-  font-weight: 600;
-  font-style: italic;
-}
-
-header .wrapper {
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  place-items: flex-start;
-  -webkit-flex-wrap: wrap;
-  -ms-flex-wrap: wrap;
-  flex-wrap: wrap;
-}
-
-nav {
-  width: 100%;
-  font-size: 24px;
-  top: 14px;
-  font-style: normal;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 3rem;
-}
-
-.mobile-wrapper {
-  display: none;
-}
-
-.mobile-nav-toggle {
-  background: transparent;
-  font-size: 48px;
-  left: 0;
-  border: 0;
-}
-
-.mobile-nav-toggle svg {
-  color: white;
-}
-
-.mobile-nav {
-  background: -webkit-linear-gradient(45deg, rgba(22,32,82,1) 0%, rgba(70,22,31,1) 100%);
-  background: -moz-linear-gradient(45deg, rgba(22,32,82,1) 0%, rgba(70,22,31,1) 100%);
-  background: -o-linear-gradient(45deg, rgba(22,32,82,1) 0%, rgba(70,22,31,1) 100%);
-  background: linear-gradient(45deg, rgba(22,32,82,1) 0%, rgba(70,22,31,1) 100%);
-  font-size: 1.2rem;
+header .container .mobileBtn {
   position: fixed;
-  top: 0;
-  left: 0;
+  float: right;
   right: 0;
-  height: 0;
-  bottom: 0;
-  z-index: 5;
+  margin: 1rem;
+  display: none;
+  z-index: 999;
+}
+
+header .container .mobile-nav {
+  position: fixed;
+  background: #1D1B26;
+  padding: 0rem 1rem;
+  width: 100%;
+  height: 210px;
+  z-index: 10;
+  font-size: 1.5rem;
   display: none;
 }
 
-.mobile-nav ul {
-  top: 1rem;
+header .container .mobile-nav ul li {
+  list-style-type: none;
+  padding: 1rem 0 0.5rem;
 }
 
-.mobile-nav ul li {
-  margin-bottom: 1rem;
+header .container .mobile-nav ul li a {
+  font-weight: 100;
+}
+
+header .container .wrapper {
+	display: -webkit-box;
+	display: -webkit-flex;
+	display: -moz-box;
+	display: -ms-flexbox;
+	display: flex;
+	padding-left: 10px;
+	padding-right: 15px;
+	padding-top: 5px;
+	padding-bottom: 5px;
+}
+
+header .container .wrapper .company {
+	font-weight: 700;
+	font-style: italic;
+}
+
+header .container .wrapper .desktop-nav nav {
+	width: 100%;
+	font-size: 24px;
+	top: 14px;
+	font-style: normal;
+}
+
+header .container .wrapper .desktop-nav nav a {
+	display: inline-block;
+	padding: 0 3rem;
 }
 
 @media (max-width: 64em) {
-  header {
-    line-height: 1.2;
-    max-height: 100vh;
-  }
-
-  header .wrapper {
-    display: none;
-  }
-
-  .mobile-wrapper {
-    position: fixed;
+	header .container .wrapper .desktop-nav {
+		display: none;
+	}
+  header .container .mobileBtn {
     display: block;
-    right: 0;
-    top: 0;
-    padding: 5px;
-    z-index: 5;
   }
 }
 </style>
