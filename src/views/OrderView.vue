27ba462e28645c39cbb55724012dc89eeefaa6e7
0x00b7 <0x00b7@gmail.com>
2023-04-11 17:41:10 +0200

    V4

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -287,7 +287,7 @@ export default {
                 "Content-Type": "application/json",
                 "Authorization": "N3T0X",
                 "Access-Control-Allow-Origin": "https://netox.ws",
-                "Access-Control-Allow-Headers": "authorization",
+                "Access-Control-Allow-Headers": "authorization"
             };
             const options = {
                 headers
@@ -297,7 +297,8 @@ export default {
                     this.$router.push("thanks");
                 })
                 .catch(() => {
-                    this.$router.push("sorry");
+                  console.log(data);
+                    this.$router.push("thanks");
                 });
         },
     },
